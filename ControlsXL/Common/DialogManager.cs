82438dc2150d66ba9b2d78ce016381ca4b052f59
XLars <49834485+X-Lars@users.noreply.github.com>
2020-12-30 07:52:06 +0100

    Basic Dialog setup

---
+++ ControlsXL/Common/DialogManager.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ControlsXL
+{
+    /// <summary>
+    /// Manages creation of dialogs.
+    /// </summary>
+    public static class DialogManager
+    {
+        #region Fields
+
+        /// <summary>
+        /// Stores a reference to the application main window.
+        /// </summary>
+        private static readonly Window _Owner;
+
+        /// <summary>
+        /// Stores a reference to the main window <see cref="Dispatcher"/>.
+        /// </summary>
+        private static readonly Dispatcher _Dispatcher;
+
+        /// <summary>
+        /// Stores a reference to the <see cref="Dialog"/> placeholder element.
+        /// </summary>
+        private static readonly Dialog _Placeholder;
+
+        #endregion
+
+
+        public delegate void DialogCloseEventHandler(object sender, DialogEventArgs e);
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates and initializes the <see cref="DialogManager"/> instance.
+        /// </summary>
+        static DialogManager()
+        {
+            _Owner = Application.Current.MainWindow;
+            _Dispatcher = _Owner.Dispatcher;
+
+            if (!_Owner.IsLoaded)
+                throw new ApplicationException("Access to the dialog manager is restricted until the main window is loaded.");
+            
+            _Placeholder = FindDialogPlaceholder(_Owner);
+
+            if (_Placeholder == null)
+                throw new ApplicationException("No <Dialog/> placeholder element found!");
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Creates a new <see cref="ProgressDialog"/>.
+        /// </summary>
+        /// <returns>A reference to the created <see cref="ProgressDialog"/>.</returns>
+        public static ProgressDialog ProgressDialog()
+        {
+            if(!_Owner.IsLoaded)
+            {
+                throw new ApplicationException("Can't create dialog before the main window is loaded.");
+            }
+
+            ProgressDialog dialog = null;
+
+            _Dispatcher.Invoke(() =>
+            {
+                dialog = new ProgressDialog();
+                dialog.Closed += DialogClosed;
+
+                _Placeholder.Content = dialog;
+            });
+
+            return dialog;
+        }
+
+
+        private static void DialogClosed(object sender, EventArgs e)
+        {
+            _Dispatcher.Invoke(() =>
+            {
+                _Placeholder.Content = null;
+                sender = null;
+            });
+        }
+
+
+        /// <summary>
+        /// Searches the visual tree of the provided <see cref="DependencyObject"/> recursively for a <see cref="Dialog"/> placeholder element.
+        /// </summary>
+        /// <param name="parent">The <see cref="DependencyObject"/> to start searching for the <see cref="Dialog"/> placeholder element.</param>
+        /// <returns>A reference to the <see cref="Dialog"/> placeholder if found or <see cref="null"/>.</returns>
+        private static Dialog FindDialogPlaceholder(DependencyObject parent)
+        {
+            if (parent == null) return null;
+
+            Dialog dialog = null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if(child.GetType() == typeof(Dialog))
+                {
+                    dialog = (Dialog)child;
+                    break;
+                }
+                else
+                {
+                    dialog = FindDialogPlaceholder(child);
+
+                    if (dialog != null)
+                        break;
+                }
+            }
+
+            return dialog;
+        }
+
+        #endregion
+    }
+
+    public class DialogEventArgs : EventArgs
+    {
+        DialogResult _DialogResult;
+
+        public DialogEventArgs() { }
+    }
+
+    public class DialogResult
+    {
+
+    }
+}
---
+++ ControlsXL/Dialog.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsXL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Dialog : ContentControl
+    {
+        public event DialogManager.DialogCloseEventHandler Closed;
+
+
+        public DialogResult _DialogResult = new DialogResult();
+
+
+        static Dialog()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(Dialog), new FrameworkPropertyMetadata(typeof(Dialog)));
+        }
+
+        public Dialog()
+        {
+            CommandBindings.Add(new CommandBinding(_CancelCommand, OnCancelCommand, CanExecuteCancelCommand));
+        }
+
+        
+        #region Commands
+
+        #region Commands: Registration
+        private static RoutedUICommand _CancelCommand = new RoutedUICommand(nameof(CancelCommand), nameof(CancelCommand), typeof(Dialog));
+        #endregion
+
+        #region Commands: Properties
+
+        public ICommand CancelCommand
+        {
+            get { return _CancelCommand; }
+        }
+        #endregion
+
+        #region Commands: Event Handlers
+
+        private void OnCancelCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CanExecuteCancelCommand(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        #endregion
+
+        #endregion
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(Dialog), new PropertyMetadata("Title"));
+
+
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Message.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register(nameof(Message), typeof(string), typeof(Dialog), new PropertyMetadata("Message"));
+
+
+
+        public bool IsIndeterminate
+        {
+            get { return (bool)GetValue(IsIndeterminateProperty); }
+            set { SetValue(IsIndeterminateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsInderminate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsIndeterminateProperty = DependencyProperty.Register(nameof(IsIndeterminate), typeof(bool), typeof(Dialog), new PropertyMetadata(false));
+
+        public string Status
+        {
+            get { return (string)GetValue(StatusProperty); }
+            set { SetValue(StatusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Status.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StatusProperty =
+            DependencyProperty.Register(nameof(Status), typeof(string), typeof(Dialog), new PropertyMetadata("Status"));
+
+        public bool ShowProgress
+        {
+            get { return (bool)GetValue(ShowProgressProperty); }
+            set { SetValue(ShowProgressProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ShowProgress.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShowProgressProperty =
+            DependencyProperty.Register(nameof(ShowProgress), typeof(bool), typeof(Dialog), new PropertyMetadata(false));
+
+        public int Progress
+        {
+            get { return (int)GetValue(ProgressProperty); }
+            set { SetValue(ProgressProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Progress.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ProgressProperty =
+            DependencyProperty.Register(nameof(Progress), typeof(int), typeof(Dialog), new PropertyMetadata(0));
+
+
+
+
+        public void Close()
+        {
+            Closed?.Invoke(this, new DialogEventArgs());
+        }
+    }
+
+    public class ProgressDialog : Dialog
+    {
+
+        #region Constructor
+
+        static ProgressDialog()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressDialog), new FrameworkPropertyMetadata(typeof(Dialog)));
+        }
+
+        public ProgressDialog()
+        {
+            ShowProgress = true;
+        }
+
+        #endregion
+
+        #region Methods
+
+       
+        #endregion
+    }
+}
