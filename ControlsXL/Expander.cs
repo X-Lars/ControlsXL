1ee38d121acae8f98d09b4b2b1eb802c0efd5338
XLars <49834485+X-Lars@users.noreply.github.com>
2021-01-19 15:26:04 +0100

    Added SearchTextBox

--- ControlsXL/Expander.cs
+++ ControlsXL/Expander.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ControlsXL
 {
-   
+
 
     public class Expander : HeaderedContentControl
     {
@@ -23,10 +12,10 @@ namespace ControlsXL
 
         static Expander()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(Expander), new FrameworkPropertyMetadata(typeof(Expander)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ControlsXL.Expander), new FrameworkPropertyMetadata(typeof(ControlsXL.Expander)));
         }
 
-        public Expander()
+        public Expander() : base()
         {
             CommandBindings.Add(new CommandBinding(ToggleStateCommand, OnToggleStateCommand));
         }
@@ -38,7 +27,7 @@ namespace ControlsXL
 
         #region Commands: Registration
 
-        private static RoutedUICommand _ToggleStateCommand = new RoutedUICommand(nameof(ToggleStateCommand), nameof(ToggleStateCommand), typeof(Expander));
+        private static ICommand _ToggleStateCommand = new RoutedUICommand(nameof(ToggleStateCommand), nameof(ToggleStateCommand), typeof(ControlsXL.Expander));
 
 
 
@@ -49,19 +38,20 @@ namespace ControlsXL
         }
 
         // Using a DependencyProperty as the backing store for HeaderAlignment.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty HeaderAlignmentProperty = DependencyProperty.Register(nameof(HeaderAlignment), typeof(HorizontalAlignment), typeof(Expander), new PropertyMetadata(HorizontalAlignment.Left));
+        public static readonly DependencyProperty HeaderAlignmentProperty = DependencyProperty.Register(nameof(HeaderAlignment), typeof(HorizontalAlignment), typeof(ControlsXL.Expander), new PropertyMetadata(HorizontalAlignment.Left));
 
 
 
+        
         public HorizontalAlignment HeaderButtonAlignment
         {
-            get { return (HorizontalAlignment)GetValue(ButtonAlignmentProperty); }
-            set { SetValue(ButtonAlignmentProperty, value); }
+            get { return (HorizontalAlignment)GetValue(HeaderButtonAlignmentProperty); }
+            set { SetValue(HeaderButtonAlignmentProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for ButtonAlignment.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ButtonAlignmentProperty = DependencyProperty.Register(nameof(HeaderButtonAlignment), typeof(HorizontalAlignment), typeof(Expander), new PropertyMetadata(HorizontalAlignment.Right));
-
+        // Using a DependencyProperty as the backing store for HeaderButtonAlignment.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderButtonAlignmentProperty =
+            DependencyProperty.Register(nameof(HeaderButtonAlignment), typeof(HorizontalAlignment), typeof(ControlsXL.Expander), new PropertyMetadata(HorizontalAlignment.Right));
 
 
 
@@ -95,7 +85,7 @@ namespace ControlsXL
         /// <summary>
         /// Registers the property to set the <see cref="Expander"/> state.
         /// </summary>
-        public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register("IsExpanded", typeof(bool), typeof(Expander), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register(nameof(IsExpanded), typeof(bool), typeof(ControlsXL.Expander), new PropertyMetadata(true));
 
 
         #endregion
---
+++ ControlsXL/SearchTextBox.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsXL
+{
+   
+    public class SearchTextBox : TextBox
+    {
+        #region Constructor
+
+        static SearchTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SearchTextBox), new FrameworkPropertyMetadata(typeof(SearchTextBox)));
+        }
+
+        public SearchTextBox() : base()
+        {
+            CommandBindings.Add(new CommandBinding(ClearCommand, OnClearCommand));
+        }
+
+       
+        #endregion
+
+        #region Commands
+        #region Commands: Registration
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static RoutedUICommand _ClearCommand = new RoutedUICommand(nameof(ClearCommand), nameof(ClearCommand), typeof(SearchTextBox));
+
+        #endregion
+        #region Commands: Properties
+
+        /// <summary>
+        /// Gets the command 
+        /// </summary>
+        public static ICommand ClearCommand
+        {
+            get { return _ClearCommand; }
+        }
+
+        #endregion
+        #region Commands: Handlers
+
+        private void OnClearCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            Text = string.Empty;
+        }
+
+        #endregion
+        #endregion
+
+        #region Dependency Properties
+        #region Dependency Properties: Registration
+
+        /// <summary>
+        /// Registers the property to set the <see cref="SearchTextBox"/> text hint.
+        /// </summary>
+        public static readonly DependencyProperty TextHintProperty = DependencyProperty.Register(nameof(TextHint), typeof(string), typeof(SearchTextBox), new UIPropertyMetadata("Search..."));
+
+        #endregion
+        #region Dependency Properties: Implementation
+
+
+        /// <summary>
+        /// Gets or sets the text hint of the <see cref="SearchTextBox"/>.
+        /// </summary>
+        public string TextHint
+        {
+            get { return (string)GetValue(TextHintProperty); }
+            set { SetValue(TextHintProperty, value); }
+        }
+
+        #endregion
+        #endregion
+
+        
+    }
+}
