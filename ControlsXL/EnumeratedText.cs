39e9e469cc6adc69009719f3ceaff461a256ae3e
XLars <xlars@xlars.com>
2022-02-11 17:31:49 +0100

    Added Enumerated Text Control

--- ControlsXL/EnumeratedText.cs
+++ ControlsXL/EnumeratedText.cs
@@ -1,58 +1,25 @@
-﻿using ControlsXL.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ControlsXL
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:ControlsXL"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:ControlsXL;assembly=ControlsXL"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:PlainTextSpinner/>
-    ///
-    /// </summary>
-    public class EnumeratedText : TextBox
+    [TemplatePart(Name = "PART_Content", Type = typeof(TextBlock))]
+    public class EnumeratedText : Control
     {
 
-        private int _Min = 0;
-        private int _Max = 0;
-        private readonly List<string> _Values = new();
+        private int Min = 0;
+        private int Max = 0;
+        private int Index = 0;
+
+        private readonly Dictionary<int, KeyValuePair<object, string>> EnumerationValues = new();
+
+        private Type EnumerationType;
+        private TextBlock _Text;
+        private Border _Border;
 
         static EnumeratedText()
         {
@@ -62,14 +29,26 @@ namespace ControlsXL
 
         public EnumeratedText() : base()
         {
-            AcceptsReturn = false;
-            AcceptsTab = false;
+            Focusable = true;
+            IsTabStop = true;
         }
 
-        public static readonly DependencyProperty SelectedIndexProperty = DependencyProperty.Register(nameof(SelectedIndex), typeof(int), typeof(EnumeratedText), new PropertyMetadata(0, SelectedIndexChanged));
-        public static readonly DependencyProperty ValueProviderProperty = DependencyProperty.Register(nameof(ValueProvider), typeof(Enum), typeof(EnumeratedText), new PropertyMetadata(null, ValueProviderChanged));
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _Text = GetTemplateChild("PART_Content") as TextBlock;
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(object), typeof(EnumeratedText), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ValueProviderChanged));
         public static readonly DependencyProperty PrefixProperty = DependencyProperty.Register(nameof(Prefix), typeof(string), typeof(EnumeratedText), new UIPropertyMetadata(string.Empty));
         public static readonly DependencyProperty SuffixProperty = DependencyProperty.Register(nameof(Suffix), typeof(string), typeof(EnumeratedText), new UIPropertyMetadata(string.Empty));
+
+        public string Text
+        {
+            get => _Text.Text;
+            private set => _Text.Text = value;
+        }
 
         public string Prefix
         {
@@ -83,66 +62,74 @@ namespace ControlsXL
             set { SetValue(SuffixProperty, value); }
         }
 
-        public int SelectedIndex
+        private int SelectedIndex
         {
-            get { return (int)GetValue(SelectedIndexProperty); }
-            set 
+            get => Index;
+
+            set
             {
-                if (value < _Min)
-                    value = _Min;
-
-                if(value > _Max)
-                    value = _Max;
-
-                SetValue(SelectedIndexProperty, value); 
-            }
-        }
-
-
-        private static void SelectedIndexChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            EnumeratedText text = (EnumeratedText)d;
-
-            if(text.ValueProvider.GetType().IsEnum)
-            {
-
-                text.Text = text._Values[(int)e.NewValue];
-            }
-        }
-
-        private static void ValueProviderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            EnumeratedText text = (EnumeratedText)d;
-
-            if (e.NewValue != null)
-            {
-                if(e.NewValue.GetType().IsEnum)
+                if (value != Index)
                 {
-                    text._Values.Clear();
+                    if (value < Min)
+                        value = Min;
 
-                    for(int i = 0; i < Enum.GetValues(text.ValueProvider.GetType()).Length; i++)
-                    {
-                        text._Values.Add((string)TypeDescriptor.GetConverter(e.NewValue).ConvertTo(Enum.GetValues(e.NewValue.GetType()).GetValue(i), typeof(string)));
-                    }
-                    
-                    text._Min = 0;
-                    text._Max = Enum.GetValues(text.ValueProvider.GetType()).Length - 1;
+                    if (value > Max)
+                        value = Max;
 
-                    text.Text = text._Values[text.SelectedIndex];
+                    Index = value;
+                    Text = EnumerationValues[value].Value;
+                    Value = EnumerationValues[value];
                 }
             }
         }
 
-        public Enum ValueProvider
+        public object Value
         {
-            get { return (Enum)GetValue(ValueProviderProperty); }
-            set { SetValue(ValueProviderProperty, value); }
+            get => GetValue(ValueProperty);
+            set => SetValue(ValueProperty, EnumerationValues[SelectedIndex].Key);
+        }
+
+
+        private static void ValueProviderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                EnumeratedText instance = (EnumeratedText)d;
+
+                if (e.NewValue.GetType() != instance.EnumerationType)
+                {
+                    if (e.NewValue.GetType().IsEnum)
+                    {
+                        instance.EnumerationValues.Clear();
+                        instance.EnumerationType = e.NewValue.GetType();
+                        instance.Max = Enum.GetValues(instance.EnumerationType).Length - 1;
+
+                        for (int index = 0; index <= instance.Max; index++)
+                        {
+                            object value = Convert.ChangeType(Enum.GetValues(instance.EnumerationType).GetValue(index), instance.EnumerationType);
+                            string description = (string)TypeDescriptor.GetConverter(e.NewValue).ConvertTo(Enum.GetValues(instance.EnumerationType).GetValue(index), typeof(string));
+
+                            KeyValuePair<object, string> enumValue = new KeyValuePair<object, string>(value, description);
+                            instance.EnumerationValues.Add(index, enumValue);
+                        }
+
+                        instance.Text = instance.EnumerationValues[instance.SelectedIndex].Value;
+                    }
+                }
+            }
+        }
+
+        #region Event Handlers
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+            Keyboard.Focus(this);
         }
 
         protected override void OnPreviewKeyDown(KeyEventArgs e)
         {
-            bool isCTRLKeyDown = e.KeyboardDevice.IsKeyDown(Key.LeftCtrl) ||
-                                 e.KeyboardDevice.IsKeyDown(Key.RightCtrl);
+            bool CTRL_KEY_DOWN = e.KeyboardDevice.IsKeyDown(Key.RightCtrl) || e.KeyboardDevice.IsKeyDown(Key.LeftCtrl);
 
             switch (e.Key)
             {
@@ -150,63 +137,63 @@ namespace ControlsXL
                 case Key.Up:
                 case Key.OemPlus:
 
-                    SelectedIndex += isCTRLKeyDown ? 10 : 1;
-
-                    SelectAll();
-                    e.Handled = true;
+                    SelectedIndex += CTRL_KEY_DOWN ? 10 : 1;
                     break;
 
                 case Key.Subtract:
                 case Key.Down:
                 case Key.OemMinus:
 
-                        SelectedIndex -= isCTRLKeyDown ? 10 : 1;
-
-                    SelectAll();
-                    e.Handled = true;
-                    break;
-
-                case Key.PageUp:
-
-                    SelectedIndex = _Max;
-
-                    SelectAll();
-                    e.Handled = true;
+                    SelectedIndex -= CTRL_KEY_DOWN ? 10 : 1;
                     break;
 
                 case Key.PageDown:
+                case Key.End:
 
-                    SelectedIndex = _Min;
+                    SelectedIndex = Max;
+                    break;
 
-                    SelectAll();
-                    e.Handled = true;
+                case Key.PageUp:
+                case Key.Home:
+
+                    SelectedIndex = Min;
                     break;
 
                 case Key.Tab:
 
-                    MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
-                    break;
+                    if (e.KeyboardDevice.IsKeyDown(Key.LeftShift) || e.KeyboardDevice.IsKeyDown(Key.RightShift) || e.KeyboardDevice.IsKeyToggled(Key.CapsLock))
+                    {
+                        MoveFocus(new TraversalRequest(FocusNavigationDirection.Previous));
+                    }
+                    else
+                    {
+                        MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+                    }
 
-                case Key.Enter:
-
-                    SelectAll();
                     break;
             }
+
+            e.Handled = true;
         }
+
         protected override void OnMouseWheel(MouseWheelEventArgs e)
         {
             base.OnMouseWheel(e);
 
             int distance = e.Delta / 100;
 
-            if(distance > 0)
+            if (distance > 0)
             {
-                SelectedIndex--;
+                if (SelectedIndex > Min)
+                    SelectedIndex--;
             }
             else
             {
-                SelectedIndex++;
+                if (SelectedIndex < Max)
+                    SelectedIndex++;
             }
         }
+
+        #endregion
     }
 }
